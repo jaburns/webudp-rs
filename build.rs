0a1867a6d014efd58159775587479d16c9d31ec3
jaburns <j@jaburns.net>
2021-03-31 23:07:34 -0600

    cmake build directly in build.rs

--- build.rs
+++ build.rs
@@ -1,28 +1,28 @@
-extern crate bindgen;
+use bindgen;
+use cmake;
 
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let mut lib_path = PathBuf::from(file!());
-    lib_path.pop();
-    lib_path.push("WebUDP");
-    lib_path.push("build");
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    cmake::build("WebUDP");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let lib_path = out_path.join("build");
+
     println!("cargo:rustc-link-search={}", lib_path.display());
     println!("cargo:rustc-link-lib=WuHost");
     println!("cargo:rustc-link-lib=Wu");
     println!("cargo:rustc-link-lib=ssl");
     println!("cargo:rustc-link-lib=crypto");
-    println!("cargo:rerun-if-changed=wrapper.h");
 
-    let bindings = bindgen::Builder::default()
+    bindgen::Builder::default()
         .header("wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
-        .expect("Unable to generate bindings");
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
+        .expect("Unable to generate bindings")
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }
